7b47965a87c8339905cc548b1e74ef0c34029cd9
AkhnazmFirmansyah <akhnazmfirmansyah@gmail.com>
2025-08-26 16:42:38 +0700

    PBO_pertemuan1

---
+++ Malikproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.malikproject1;
+
+/**
+ *
+ * @author acer
+ */
+public class Malikproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama  : Akhnaz Malik Firmansyah \nNPM   : 2440506072 \nAlamat: Desa Pakis \nNo_HP : 085700915583");
+    }
+}
