fee767d360d6f8b6329395c1d06923f84a4291ac
AkhnazmFirmansyah <akhnazmfirmansyah@gmail.com>
2025-11-04 16:25:51 +0700

    Abstraksi dan Kelas Abstrak

---
+++ tugaspraktikumpbo_9/Anjing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugaspraktikumpbo_9;
+
+/**
+ *
+ * @author acer
+ */
+class Anjing extends Hewan {
+    @Override
+    void suara() {
+        System.out.println("Anjing bersuara guk guk");
+    }
+}
